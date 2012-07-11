91881f788e69e96f23ea07bc658c498d380d3775
Dominik Eigner <domi.eigner@web.de>
2012-07-11 10:33:26 +0200

    test

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
-var sys = require('util');
-var http = require('http');
+var sys;
+
+sys = require('util');
+var http;
+http = require('http');
+
+sys.puts('server Starting');
 
 http.createServer(function (req, res) {
-  setTimeout(function () {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World ...\n');
-  }, 2000);
+    setTimeout(function () {
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.end("Hello World ...\n");
+    }, 2000);
 }).listen(8000);
 
 sys.puts('Server running at http://127.0.0.1:8000/');
+
+http.createServer(function(req,res) {
+
+        socket.write(req);
+
+}).listen(9000);
+
