93f5a2ba7f9e076d70a983f8d4c8e2a48466bfce
Dominik Eigner <domi.eigner@web.de>
2012-07-11 12:49:57 +0200

    Client test

--- server.js
+++ server.js
@@ -4,9 +4,11 @@
  * Date: 11.07.12
  * Time: 11:43
  */
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(8000);
+var utils = require('utils');
 
 io.sockets.on('connection', function (socket) {
+    utils.puts('Hello called');
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
