fd6cf1ea1d1c35ebf9305140a19bab216985b25e
maxargyo <max.argyo@web.de>
2012-07-11 12:15:19 +0200

    test

--- server.js
+++ server.js
@@ -10,5 +10,6 @@ io.sockets.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
+        /*TEST*/
     });
 });
