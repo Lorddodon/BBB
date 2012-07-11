c70d1181a2b1b40072a6881f07e5d3cec264b324
Dominik Eigner <domi.eigner@web.de>
2012-07-11 12:14:10 +0200

    Client test

--- test.js
+++ test.js
@@ -6,11 +6,14 @@ http = require('http');
 sys.puts('server Starting');
 
 http.createServer(function (req, res) {
-  setTimeout(function () {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World ...\n');
-  }, 2000);
+    setTimeout(function () {
+        sys.puts('Hello called');
+        res.writeHead(200, {'Content-Type':'text/plain'});
+        res.end("test");
+    }, 2000);
 }).listen(8000);
 
 sys.puts('Server running at http://127.0.0.1:8000/');
+
 /*TEST*/
+
