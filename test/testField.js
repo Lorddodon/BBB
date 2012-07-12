62ae783051fa7d59cc4102313a01ea447594700b
maxargyo <max.argyo@web.de>
2012-07-12 15:44:32 +0200

    TEEEEEST

---
+++ test/testField.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: chip
+ * Date: 12.07.12
+ * Time: 15:36
+ * To change this template use File | Settings | File Templates.
+ */
+var assert = require('assert');
+var fieldFactory = require('../graph/Field');
+
+function createTest() {
+    var field = fieldFactory.Field(9,9);
+    assert.equal(field.width, 9, "NOT");
+}
