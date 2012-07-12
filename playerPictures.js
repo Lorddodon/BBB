f9f3c1b6602916411c8796f2c96164feea9f8df5
Amrei <Alyss123@web.de>
2012-07-12 14:03:38 +0200

    sprites

--- playerPictures.js
+++ playerPictures.js
@@ -6,42 +6,23 @@
  * To change this template use File | Settings | File Templates.
  */
 
-//var index = 10;
-
-/*function setPictureIndex(i)
-{
-   index = i;
-}*/
 
 
 function drawPicture(index)
 {
-    var canvas, context, image, width, height, xpos = 0, ypos = 0, numFrames = 60, frameSize = 30;
+    var canvas, context, image, width, height, xpos = 0, ypos = 0, numFrames = 15, frameSize = 29;
     image = new Image();
     image.src = "./spritesheet.png";
     image.onload = function() {
         width = image.width;
         height = image.height;
         canvas = document.getElementById("canvas");
-        var i = index;
-        while(i > 0){
-            xpos = xpos + frameSize;
-            i = i - 1;
-        }
-        if(index >= numFrames) {
-            xpos = 0;
-            ypos = 0;
-            index = 0;
-        }
-        else if(xpos + frameSize > width) {
-            xpos = 0;
-            ypos += frameSize;
-        }
+        ypos = (index-(index%numFrames))/numFrames*frameSize;
+        xpos = (index%numFrames)*frameSize;
         context = canvas.getContext("2d");
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.drawImage(image, xpos, ypos, frameSize, frameSize, 0, 0, frameSize, frameSize);
-
     }
 }
 
-setInterval(drawPicture(1),1000);
+drawPicture(0);
