d8ee69990c1497e1bd173d8ee837738014f8bd71
Reinhard Alischer <reinhard@alischer.eu>
2012-07-12 22:09:26 +0200

    updated client, server and css

--- Client/client.js
+++ Client/client.js
@@ -11,10 +11,13 @@ var player = undefined;
 var otherplayer = undefined;
 var graph = undefined;
 
+var bombs = [];
+
 socket.on('graph', function(data){
     console.log('graph received..');
     graph = data['graph'];
     /*TODO: zeichne Feld nach graphen*/
+    drawBackgroundGrid();
 });
 
 socket.on('identity', function(data){
@@ -59,10 +62,26 @@ socket.on('show_flame',function(data){
 socket.on('bomb_placed',function(data){
     /*TODO: zeichne an position eine bombe*/
     var bomb = data['bomb'];
+    bombs.push(bomb);
+    drawBomb(bomb.x,bomb.y);
 });
 
 socket.on('bomb_explode',function(data){
-   /*TODO: bombe löschen*/
+    function remove (arr, index) {
+        var arr2 = arr.splice(index).splice(1);
+        return arr.concat(arr2);
+    };
+    var index = -1;
+    for(var i = 0; i < bombs.length; i++) {
+        if(bombs[i].id == data['id']) {
+            index = i;
+            break;
+        }
+    }
+    var bomb = bombs[index];
+    if(index > -1)
+        remove(bombs,index);
+    removeBomb(bomb.x,bomb.y);
 });
 
 socket.on('delete_entities',function(data){
@@ -96,6 +115,45 @@ Mousetrap.bind('space', function() {
     socket.emit("drop_bomb",{id:player.id})
     console.log("drop_bomb")
 });
+
+function drawBomb(xpos, ypos){
+    var index = 51;
+    var canvas, context, width, height, x = 0, y = 0, numFrames = 15, frameSize = 29;
+    var mul = 30;image = new Image();
+    image.src = "./spritesheet.png";
+    image.onload = function() {
+        width = image.width;
+        height = image.height;
+        canvas = document.getElementById("bombs");
+        y = (index-(index%numFrames))/numFrames*frameSize;
+        x = (index%numFrames)*frameSize;
+        context = canvas.getContext("2d");
+        context.drawImage(image, x, y, frameSize, frameSize, xpos*mul+4, ypos*mul+4, frameSize, frameSize);
+    }
+}
+
+function removeBomb(xpos, ypos) {
+    var canvas, context;
+    var mul = 30;
+    canvas = document.getElementById("bombs");
+    context = canvas.getContext("2d");
+    context.clearRect(xpos*mul+4, ypos*mul+4, 30, 30);
+}
+
+function drawBackgroundGrid() {
+    var canvas, context;
+    canvas = document.getElementById("background");
+    context = canvas.getContext("2d");
+    for(var i = 0; i <= 270; i+=30) {
+        if(i % 30 == 0) {
+            context.moveTo(i,0);
+            context.lineTo(i,270);
+            context.moveTo(0,i);
+            context.lineTo(270,i);
+            context.stroke();
+        }
+    }
+}
 
 function drawPicture(index,xpos,ypos,xposold,yposold)
 {
