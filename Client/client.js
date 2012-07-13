ab0593f03c5dc5830195a0dc0797f488b4d311b7
Amrei <Alyss123@web.de>
2012-07-13 10:06:25 +0200

    death - window

--- Client/client.js
+++ Client/client.js
@@ -107,6 +107,10 @@ socket.on('delete_entities',function(data){
 });
 
 socket.on('players_died',function(data){
+    if(data.length == 2)
+        window.alert("Draw");
+    else
+        window.alert("Game over. Player "+ data[0].entity.id + "died.");
     console.log(data);
 });
 
