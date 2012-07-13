d8c37edb3b416f2c0e1952ab0fbea209b3fb8abb
maxargyo <max.argyo@web.de>
2012-07-13 12:05:50 +0200

    client.html angepasst server fixed. blastTo methode

--- Client/client.js
+++ Client/client.js
@@ -177,7 +177,7 @@ function clearFlameLayer() {
     var canvas, context;
     canvas = document.getElementById('flames');
     context = canvas.getContext('2d');
-    context.clearRect(0,0,270,270);
+    context.clearRect(0,0,graph.width * 30,graph.height * 30);
 }
 
 function drawFlame(xpos, ypos) {
